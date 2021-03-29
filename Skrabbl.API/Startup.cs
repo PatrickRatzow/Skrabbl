7e7417c1a405b488cd3bfc2034d870c9f9ce5931
Patrick Ratzow <patrickratzow@hotmail.dk>
2021-03-29 15:36:39 +0200

    Revert "WebSocket testing"

--- Skrabbl.API/Startup.cs
+++ Skrabbl.API/Startup.cs
@@ -10,7 +10,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Skrabbl.API.Hubs;
 
 namespace Skrabbl.API
 {
@@ -28,7 +27,6 @@ namespace Skrabbl.API
         {
             services.AddSpaStaticFiles(options => { options.RootPath = "wwwroot"; });
             services.AddControllers();
-            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,7 +46,6 @@ namespace Skrabbl.API
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
-                endpoints.MapHub<ChatHub>("/chatHub");
             });
 
             app.UseSpaStaticFiles();
