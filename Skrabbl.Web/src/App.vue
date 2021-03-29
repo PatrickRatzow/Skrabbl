b4bb635b6644e5c346bc3b80e5dfcba626629d6b
SleepyC4 <nikolajcarlslund@gmail.com>
2021-03-29 15:35:10 +0200

    WebSocket testing

--- Skrabbl.Web/src/App.vue
+++ Skrabbl.Web/src/App.vue
@@ -1,16 +1,19 @@
 <template>
     <div id="app">
         <Home msg=":) world!" />
+        <WebSocketItem/>
     </div>
 </template>
 
 <script>
     import Home from './components/Home.vue';
+    import WebSocketItem from './components/WebSocketItem.vue';
 
     export default {
         name: 'app',
         components: {
-            Home
+            Home,
+            WebSocketItem
         }
     };
 </script>
