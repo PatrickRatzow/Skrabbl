6a26ad5158a5de018a5137eca9c14ec58ff4f586
Patrick Ratzow <patrickratzow@hotmail.dk>
2021-03-30 09:31:40 +0200

    feat(vue): add weather API demo

---
+++ Skrabbl.Web/src/components/WeatherItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h3>{{ formattedDate }} - {{ summary }}</h3>
+    <p>The temperature will be {{ temperature }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "WeatherItem",
+  props: {
+    date: String,
+    temperature: Number,
+    summary: String
+  },
+  computed: {
+    formattedDate() {
+      return new Date(this.date).toLocaleDateString()
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- Skrabbl.Web/src/views/Home.vue
+++ Skrabbl.Web/src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
     <Home msg="Welcome to Your Vue.js App"/>
+    <WeatherList />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Home from '@/components/Home.vue'
+import WeatherList from "@/components/WeatherList";
 
 export default {
   name: 'home',
   components: {
-    Home
+    Home,
+    WeatherList
   }
 }
 </script>
