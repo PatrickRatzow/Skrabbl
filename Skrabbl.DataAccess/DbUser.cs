6ac2b030908814b446e82afa94723102f7b36228
thorhoward <70630844+thorhoward@users.noreply.github.com>
2021-03-30 12:16:57 +0200

    Dapper

---
+++ Skrabbl.DataAccess/DbUser.cs
@@ -0,0 +1,62 @@
+﻿using Skrabbl.Model;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using Dapper;
+using System.Linq;
+using System.Diagnostics;
+
+namespace Skrabbl.DataAccess
+{
+   public class DbUser : IUser
+    {
+        private string connectionString;
+
+        public DbUser() {
+            connectionString = @"Data Source=DESKTOP-RJ2KJ7K\SQLEXPRESS;Initial Catalog = Test; Integrated Security=true";
+        }
+        // Test
+        public DbUser(String connectionStringTest) {
+            connectionString = connectionStringTest;
+        }
+
+        public bool Connect() {
+            bool connect = false;
+            using (SqlConnection connection = new SqlConnection(connectionString)) {
+                connection.Open();
+                if (connection.State == System.Data.ConnectionState.Open) {
+                    connect = true;
+                }
+            }
+            return connect;
+        }
+
+
+        public List<User> GetAllUsers()
+        {
+            string userSqlSelect = "select * from _user";
+            List<User> users = null;
+            using (SqlConnection userConnection = new SqlConnection(connectionString)) {
+                users = userConnection.Query<User>(userSqlSelect).ToList();
+                foreach (var user in users) {
+                   Debug.WriteLine(user.username);
+                }
+                
+            
+            }
+            return users;
+        }
+
+        public User GetUserById(int inputId)
+        {
+            string getUserByIdSql = "select * from _user where userId = @inputUserId";
+            User foundUser = null;
+            using (SqlConnection userConnection = new SqlConnection(connectionString))
+                foundUser = userConnection.QueryFirstOrDefault<User>(getUserByIdSql, new { inputUserId = inputId });
+            return foundUser;
+        }
+        
+    }
+
+}
---
+++ Skrabbl.DataAccess/IUser.cs
@@ -0,0 +1,14 @@
+﻿using Skrabbl.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Skrabbl.DataAccess
+{
+    interface IUser
+    {
+        List<User> GetAllUsers();
+        User GetUserById(int inputId);
+
+    }
+}
