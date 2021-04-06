a25bd9282ff1a65c8a05c637f7ca45ab8810927f
SleepyC4 <nikolajcarlslund@gmail.com>
2021-04-06 10:33:43 +0200

    feat(api): created game lobby table migration

---
+++ Skrabbl.API/Migrations/3_CreateGameLobbyTable.cs
@@ -0,0 +1,28 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Skrabbl.API.Migrations
+{
+    [Migration(3)]
+    public class CreateGameLobbyTable : Migration
+    {
+        public override void Down()
+        {
+            Delete.Table("GameLobby");
+        }
+
+        public override void Up()
+        {
+            Create.Table("GameLobby")
+                .WithColumn("GameCode").AsFixedLengthString(4).PrimaryKey()
+                .WithColumn("LobbyOwnerId").AsInt32().NotNullable();
+
+            Create.ForeignKey()
+                .FromTable("GameLobby").ForeignColumn("LobbyOwnerId")
+                .ToTable("Users").PrimaryColumn("Id");
+        }
+    }
+}
