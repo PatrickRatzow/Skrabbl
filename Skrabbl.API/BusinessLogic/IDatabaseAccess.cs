05aeb2d45054684e41ea47119ac61b1b5f3d8181
SleepyC4 <nikolajcarlslund@gmail.com>
2021-04-06 09:41:44 +0200

    feat(api): added generic DB interface

---
+++ Skrabbl.API/BusinessLogic/IDatabaseAccess.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Skrabbl.API.BusinessLogicLayer
+{
+    interface IDatabaseAccess<T>
+    {
+        T Get(int id);
+        List<T> Get();
+        int Add(T toAdd);
+        bool Put(T toUpdate);
+        bool Delete(int id);
+    }
+}
