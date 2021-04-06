76e81295a63f9c37c123e81610799c05777e2d73
SleepyC4 <nikolajcarlslund@gmail.com>
2021-04-06 10:34:22 +0200

    feat(model): added GameLobby model class

---
+++ Skrabbl.Model/GameLobby.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Skrabbl.Model
+{
+    public class GameLobby
+    {
+        public string GameCode { get; set; }
+    }
+}
