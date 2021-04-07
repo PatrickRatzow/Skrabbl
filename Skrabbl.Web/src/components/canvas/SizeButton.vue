5ab5f0208338058af6aae40da972f7b951c2853c
Patrick Ratzow <patrickratzow@hotmail.dk>
2021-04-07 13:16:53 +0200

    refactor(vue): split canvas into components

---
+++ Skrabbl.Web/src/components/canvas/SizeButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <button @click="$emit('click', size)">
+    {{ size }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "SizeButton",
+  props: {
+    size: Number
+  }
+}
+</script>
+
+<style scoped>
+  button {
+    background-color: #00d1b2;
+  }
+</style>
