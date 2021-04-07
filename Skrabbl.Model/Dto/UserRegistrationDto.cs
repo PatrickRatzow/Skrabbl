79ade175993711508ccc5bbcab2679e152cab9c9
SimonNiese <1086234@ucn.dk>
2021-04-07 13:50:10 +0200

    Added Registration UI

---
+++ Skrabbl.Model/Dto/UserRegistrationDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Skrabbl.Model.Dto
+{
+    public class UserRegistrationDto
+    {
+        [Required]
+        [MinLength(4)]
+        [MaxLength(16)]
+        public string UserName { get; set; }
+        [Required]
+        [MinLength(8)]
+        [MaxLength(64)]
+        public string Password { get; set; }
+        [Required]
+        [EmailAddress(ErrorMessage = "Invalid Email")]
+        public string Email { get; set; }
+
+    }
+}
---
+++ Skrabbl.Web/src/views/Login.vue
@@ -0,0 +1,20 @@
+<template>
+    <form>
+        <label>Username:</label><br />
+        <input x:name="inUsername" /><br />
+        <label>Password:</label><br />
+        <input x:name="inPassword" type="password"/><br />
+        <button x:name="btnLogin" onclick="runLogin">Login</button>
+    </form>
+</template>
+
+<script>
+    function runLogin() {
+
+    }
+
+
+</script>
+
+<style scoped>
+</style>
