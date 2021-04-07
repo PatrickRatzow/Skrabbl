18eef0745728281a0824ec945dd6b7d55dcfd98a
SleepyC4 <nikolajcarlslund@gmail.com>
2021-04-07 15:21:49 +0200

    feat(model): created an error class

---
+++ Skrabbl.Model/Errors/UserAlreadyHaveALobbyException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Skrabbl.Model.Errors
+{
+    public class UserAlreadyHaveALobbyException : Exception
+    {
+        public UserAlreadyHaveALobbyException()
+        {
+
+        }
+
+        public UserAlreadyHaveALobbyException(string message) : base(message)
+        {
+
+        }
+
+        public UserAlreadyHaveALobbyException(string message, Exception inner) : base(message, inner)
+        {
+
+        }
+    }
+}
