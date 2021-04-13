0977cffcccee1549b51496af729cdd2c3b0a8152
SleepyC4 <nikolajcarlslund@gmail.com>
2021-04-13 12:31:37 +0200

    feat(web): added words

---
+++ Skrabbl.Web/src/components/words/WordItem.vue
@@ -0,0 +1,18 @@
+<template>
+    <li>
+        <span>
+            {{word}}
+        </span>
+    </li>
+</template>
+
+<script>
+    export default {
+        props: {
+            word: ""
+        }
+    }
+</script>
+
+<style scoped>
+</style>
