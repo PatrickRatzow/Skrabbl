384f48aa0d11b1c3dc3eb7195a71bcef0db0064e
InvisibleOlive <70630509+InvisibleOlive@users.noreply.github.com>
2021-04-15 11:04:47 +0200

    GameServiceSpec test

---
+++ Skrabbl.API.Test/Services/GameServiceSpec.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using NUnit.Framework;
+using Skrabbl.DataAccess;
+using Skrabbl.Model;
+using System.Threading.Tasks;
+using Moq;
+using Skrabbl.API.Services;
+using Skrabbl.Model.Errors;
+using NUnit.Framework.Constraints;
+
+
+namespace Skrabbl.API.Test.Services
+{
+    class GameServiceSpec
+    {
+        [Test]
+        public async Task GetGame_Succeeds()
+        {
+            //Arrange
+            int gameiD = 3;
+            var mock = new Mock<GameRepository>();
+            mock.Setup(m => m.GetGame(It.IsAny<int>())).Returns(Task.FromResult<Game>(null));
+            var service = new GameService(mock.Object);
+
+            //Act
+            await service.GetGame(gameiD);
+
+            //Assert
+            mock.VerifyAll();
+        }
+
+        //Has not been implemented yet :)
+        [Test]
+        public Task EndTurn_Succeeds()
+        {
+            //Arrange
+            
+
+            //Act
+
+            //Assert
+        }
+    }
+}
