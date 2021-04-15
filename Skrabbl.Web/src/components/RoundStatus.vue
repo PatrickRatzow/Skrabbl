0a50ca01cad170593ed5ada7f98e991c59c1fec1
SimonNiese <1086234@ucn.dk>
2021-04-15 13:40:27 +0200

    added roundstatus component

---
+++ Skrabbl.Web/src/components/RoundStatus.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="modal is-active">
+        <div class="modal-background"></div>
+        <div class="modal-content">
+            <div class="box">
+                <h1>{{ word }}</h1><br />
+                <div v-for="player in playerscores">
+                    <h2>{{player.player}}: {{player.score}}</h2>
+                </div>
+                <button @click="click">click!</button>
+            </div>
+        </div>
+        <button class="modal-close is-large" aria-label="close"></button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "RoundStatus",
+        data() {
+            return {
+                word: "",
+                playerscores: []
+
+            }
+        },
+        methods: {
+            click() {
+                this.word = "click",
+                this.playerscores = [{ player: "player1", score: "500"}, { player: "player2", score: "400"}]
+
+            }
+        }
+
+    }
+</script>
+
+<style scoped>
+</style>
