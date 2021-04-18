b17f0a2b380b1a6bb87f75b894fe4a3d31ee713d
Patrick Ratzow <patrickratzow@hotmail.dk>
2021-04-18 23:52:11 +0200

    feat(vue): add loading circle between views
    Signed-off-by: Patrick Ratzow <patrickratzow@hotmail.dk>

--- Skrabbl.Web/src/router.js
+++ Skrabbl.Web/src/router.js
@@ -61,6 +61,5 @@ export default new Router({
             // which is lazy-loaded when the route is visited.
             component: () => import(/* webpackChunkName: "join-lobby" */ './views/JoinLobby')
         }
-       
     ]
 })
