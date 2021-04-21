8a752b8c0c47833856f72d16ac4f3e39fb3ee098
Patrick Ratzow <patrickratzow@hotmail.dk>
2021-04-21 16:36:05 +0200

    refactor(vue): remove scrollbar from ConnectionModal and center the content
    Signed-off-by: Patrick Ratzow <patrickratzow@hotmail.dk>

--- Skrabbl.Web/src/components/modals/ConnectionModal.vue
+++ Skrabbl.Web/src/components/modals/ConnectionModal.vue
@@ -1,23 +1,24 @@
 <template>
-        <div class="modal is-active">
-
-            <div class="modal-background"></div>
-            <div class="modal-content">
-                <h1 class="title has-text-white">Connections is lost</h1>
-                <h2 class="subtitle has-text-white">Try to reload your browser, or check your internet connection</h2>
+    <div class="modal is-active">
+        <div class="modal-background"></div>
+        <div class="modal-content">
+            <div class="is-flex is-justify-content-center is-align-items-center is-flex-direction-column">
+                <h1 class="title has-text-white pb-1">Connection has been lost</h1>
+                <h2 class="subtitle has-text-grey-lighter">Check your internet connection, or try to refresh your
+                    browser</h2>
             </div>
-            
         </div>
+    </div>
 </template>
 
 <script>
-    
     export default {
         name: "ConnectionModal",
-      
-
     }
 </script>
 
 <style scoped>
+.modal-content {
+    overflow: hidden;
+}
 </style>
