5bde39af0069a8180e93889df66fd20abdde2440
michelvedelhoward <michel.howard@gamil.com>
2021-04-21 12:35:21 +0200

    Userfriendly redirecting is added

--- Skrabbl.Web/src/views/Game.vue
+++ Skrabbl.Web/src/views/Game.vue
@@ -3,16 +3,16 @@
         <div class="columns mt-6 mb-6">
             <div v-if="true" class="column box pr-1 pl-1 mr-4">
                 <h2 class="subtitle">Players</h2>
-                <PointList class=" PointList"/>
-                
+                <PointList class=" PointList" />
+
             </div>
             <div class="column p-0">
                 <div class="box has-text-centered">
                     <h1 class="title word-hint">____</h1>
                 </div>
                 <div class="columns">
-                    <Canvas class="ml-3 mr-3 column box canvas"/>
-                    <ChatboxList class="mr-3 column box chatbox-list"/>
+                    <Canvas class="ml-3 mr-3 column box canvas" />
+                    <ChatboxList class="mr-3 column box chatbox-list" />
                 </div>
             </div>
         </div>
@@ -20,30 +20,37 @@
 </template>
 
 <script>
-import Canvas from "@/components/canvas/Canvas";
-import ChatboxList from "@/components/chatbox/ChatboxList";
-import PointList from "@/components/PointList";
+    import Canvas from "@/components/canvas/Canvas";
+    import ChatboxList from "@/components/chatbox/ChatboxList";
+    import PointList from "@/components/PointList";
+    import Store from '@/store/index.js'
 
-export default {
-  name: "Game",
-  components: {
-    Canvas,
-    ChatboxList,
-    PointList
-  }
-}
+
+    export default {
+        name: "Game",
+        components: {
+            Canvas,
+            ChatboxList,
+            PointList
+        },
+        mounted() {
+            if (!this.$store.getters["user/isLoggedIn"]) {
+                this.$router.push("/")
+            }
+        }
+    }
 </script>
 
 <style scoped>
-.word-hint {
-    letter-spacing: 0.2rem;
-}
+    .word-hint {
+        letter-spacing: 0.2rem;
+    }
 
-.chatbox-list {
-    max-width: 250px;
-}
+    .chatbox-list {
+        max-width: 250px;
+    }
 
-.chatbox-list, .canvas {
-    margin-bottom: 2.4rem;
-}
+    .chatbox-list, .canvas {
+        margin-bottom: 2.4rem;
+    }
 </style>
--- Skrabbl.Web/src/views/GameLobby.vue
+++ Skrabbl.Web/src/views/GameLobby.vue
@@ -1,98 +1,108 @@
 <template>
-  <div class="main is-flex is-justify-content-center is-align-items-center">
-    <div>Is connected {{ isConnected ? 'Yes' : 'False' }}</div>
-    <section class="box">
-      <section class="title has-text-centered">
-        <div class="content">
-          <p class="title">Game Lobby</p>
-          <p class="subtitle">Code: {{ lobbyCode }}</p>
-        </div>
-      </section>
-      
-      <div class="content-wrapper">
-        <section class="players">
-          <h1 class="is-size-5">Players</h1>
-          <PlayerList :players="players" />
+    <div class="main is-flex is-justify-content-center is-align-items-center">
+        <div>Is connected {{ isConnected ? 'Yes' : 'False' }}</div>
+        <section class="box">
+            <section class="title has-text-centered">
+                <div class="content">
+                    <p class="title">Game Lobby</p>
+                    <p class="subtitle">Code: {{ lobbyCode }}</p>
+                </div>
+            </section>
+
+            <div class="content-wrapper">
+                <section class="players">
+                    <h1 class="is-size-5">Players</h1>
+                    <PlayerList :players="players" />
+                </section>
+
+                <section class="settings">
+                    <!-- <h1 class="is-size-5">Settings</h1> -->
+                </section>
+            </div>
         </section>
-  
-        <section class="settings">
-          <!-- <h1 class="is-size-5">Settings</h1> -->
-        </section>   
-      </div>
-    </section>
-  </div>
+    </div>
 </template>
 
 <script>
-import { mapState } from "vuex"
-import PlayerList from "@/components/player/PlayerList";
-import SettingList from "@/components/setting/SettingList";
+    import { mapState } from "vuex"
+    import PlayerList from "@/components/player/PlayerList";
+    import SettingList from "@/components/setting/SettingList";
 
-export default {
-  name: "GameLobby",
-  computed: {
-    ...mapState("game", {
-      players: "players",
-      lobbyCode: "lobbyCode",
-    }),
-    ...mapState("signalR", {
-      isConnected: "connected"
-    })
-  },
-  components: {
-    PlayerList,
-    SettingList
-  }
-}
+    export default {
+        name: "GameLobby",
+        computed: {
+            ...mapState("game", {
+                players: "players",
+                lobbyCode: "lobbyCode",
+            }),
+            ...mapState("signalR", {
+                isConnected: "connected"
+            })
+        },
+
+        components: {
+            PlayerList,
+            SettingList
+        },
+        mounted() {
+            if (!this.$store.getters["user/isLoggedIn"]) {
+                this.$router.push("/")
+            }
+        }
+    }
 </script>
 
 <style scoped lang="scss">
-  .main {
-    min-height: 100vh;
-    
-    > section {
-      min-height: 600px;
-      min-width: 600px;
-      
-      > .title {
+    .main {
+        min-height: 100vh;
+        > section
+
+    {
+        min-height: 600px;
+        min-width: 600px;
+        > .title
+
+    {
         margin-top: -1.25rem;
         margin-left: -1.25rem;
         margin-right: -1.25rem;
-        
         border-top-right-radius: 6px;
         border-top-left-radius: 6px;
         background: #00d1b2;
-        
-        > .content {
-          padding: 3rem 1.25rem;
-          
-          > .title {
-            color: white;
-          }
-          > .subtitle {
-            color: darken(white, 5%);
-          }
-        }
-      }
+        > .content
+
+    {
+        padding: 3rem 1.25rem;
+        > .title
+
+    {
+        color: white;
     }
-  }
 
-  .content-wrapper {
-    display: flex;
+    > .subtitle {
+        color: darken(white, 5%);
+    }
 
-    > .players {
-      margin-top: -1.5rem;
-      margin-left: -1.25rem;
-      margin-bottom: -1.25rem;
-      
-      padding: 1.25rem;
-      
-      height: 100%;
-      width: 35%;
+    }
+    }
+    }
+    }
+
+    .content-wrapper {
+        display: flex;
+        > .players
+
+    {
+        margin-top: -1.5rem;
+        margin-left: -1.25rem;
+        margin-bottom: -1.25rem;
+        padding: 1.25rem;
+        height: 100%;
+        width: 35%;
     }
 
     > .settings {
-      width: 65%;
+        width: 65%;
     }
-  }
+    }
 </style>
