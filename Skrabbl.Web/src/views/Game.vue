dd7c8c0c658458f8e7a49ebedab90dda2832e3ee
InvisibleOlive <70630509+InvisibleOlive@users.noreply.github.com>
2021-04-21 10:55:46 +0200

    Method for updating word in GUI
    Takes letter and index as parameters

--- Skrabbl.Web/src/views/Game.vue
+++ Skrabbl.Web/src/views/Game.vue
@@ -3,16 +3,16 @@
         <div class="columns mt-6 mb-6">
             <div v-if="true" class="column box pr-1 pl-1 mr-4">
                 <h2 class="subtitle">Players</h2>
-                <PointList class=" PointList"/>
-                
+                <PointList class=" PointList" />
+
             </div>
             <div class="column p-0">
                 <div class="box has-text-centered">
-                    <h1 class="title word-hint">____</h1>
+                    <h1 class="title word-hint">{{ hiddenWord }}</h1>
                 </div>
                 <div class="columns">
-                    <Canvas class="ml-3 mr-3 column box canvas"/>
-                    <ChatboxList class="mr-3 column box chatbox-list"/>
+                    <Canvas class="ml-3 mr-3 column box canvas" />
+                    <ChatboxList class="mr-3 column box chatbox-list" />
                 </div>
             </div>
         </div>
@@ -20,30 +20,45 @@
 </template>
 
 <script>
-import Canvas from "@/components/canvas/Canvas";
-import ChatboxList from "@/components/chatbox/ChatboxList";
-import PointList from "@/components/PointList";
+    import Canvas from "@/components/canvas/Canvas";
+    import ChatboxList from "@/components/chatbox/ChatboxList";
+    import PointList from "@/components/PointList";
 
-export default {
-  name: "Game",
-  components: {
-    Canvas,
-    ChatboxList,
-    PointList
-  }
-}
+    export default {
+        name: "Game",
+        components: {
+            Canvas,
+            ChatboxList,
+            PointList
+        },
+        data() {
+            return {
+                hiddenWord: "____"
+            }
+        },
+        mounted() {
+            this.showPartOfWord(0, "C")
+        },
+        methods: {
+            showPartOfWord(index, letter) {
+                    this.hiddenWord = this.hiddenWord.substring(0, index) +
+                        letter + 
+                        this.hiddenWord.substring(index + 1);             
+            }
+        }
+    }
 </script>
 
 <style scoped>
-.word-hint {
-    letter-spacing: 0.2rem;
-}
+    .word-hint {
+        letter-spacing: 0.2rem;
+    }
 
-.chatbox-list {
-    max-width: 250px;
-}
+    .chatbox-list {
+        max-width: 250px;
+    }
 
-.chatbox-list, .canvas {
-    margin-bottom: 2.4rem;
-}
+    .chatbox-list, .canvas {
+        margin-bottom: 2.4rem;
+    }
 </style>
