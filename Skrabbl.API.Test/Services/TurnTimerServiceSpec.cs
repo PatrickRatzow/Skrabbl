6459a475fa77fee960feb25a77b4ae97e7710f5c
SleepyC4 <nikolajcarlslund@gmail.com>
2021-04-23 12:56:02 +0200

    fix(test): made turntimer test right

--- Skrabbl.API.Test/Services/TurnTimerServiceSpec.cs
+++ Skrabbl.API.Test/Services/TurnTimerServiceSpec.cs
@@ -46,20 +46,41 @@ namespace Skrabbl.API.Test.Services
             Assert.IsFalse(service.GetTimer(gameId).Enabled);
         }
         [Test]
-        public async Task TimerStoppedMessgae()
+        public async Task TimerElapsed_Success()
         {
             //Arrange
             int gameId = 5;
             Game game = new Game();
-            int time = 60000;
+            int time = 500;
 
             var service = new TurnTimerService();
             service.CreateTimer(gameId, time);
 
             //Act
+            service.StartTimer(gameId);
+            await Task.Delay(600);
 
             //Assert
             Assert.IsFalse(service.GetTimer(gameId).Enabled);
+        }
+
+        [Test]
+        public async Task TimerElapsedEventCalled_Success()
+        {
+            int gameId = 5;
+            Game game = new Game();
+            int time = 500;
+            bool timerHasElapsed = false;
+
+            var service = new TurnTimerService();
+            service.CreateTimer(gameId, time, timer => timerHasElapsed = true);
+            
+            //Act
+            service.StartTimer(gameId);
+            await Task.Delay(600);
+
+            //Assert
+            Assert.IsTrue(timerHasElapsed);
         }
     }
 }
