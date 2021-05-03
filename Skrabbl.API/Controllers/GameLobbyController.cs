635a1f191a94e51fd6ddd6ea7e93b1fd7c5cef5d
SleepyC4 <nikolajcarlslund@gmail.com>
2021-05-03 13:32:10 +0200

    fix: merge error

--- Skrabbl.API/Controllers/GameLobbyController.cs
+++ Skrabbl.API/Controllers/GameLobbyController.cs
@@ -55,7 +55,7 @@ namespace Skrabbl.API.Controllers
 
         [HttpPost("create/{userId}")]
         [Authorize(Policy = "HasBoughtGame")]
-        public async Task<IActionResult> Create([FromBody]List<GameSettingDto> gameSettings, int userId)
+        public async Task<IActionResult> Create(int userId, [FromBody] List<GameSettingDto> gameSettings)
 
         {
             var user = await _userService.GetUser(userId);
