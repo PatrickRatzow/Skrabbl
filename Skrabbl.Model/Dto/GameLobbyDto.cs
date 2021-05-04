e9018116df0b1aff8c19bd4849d86beb4bbbb01a
SleepyC4 <nikolajcarlslund@gmail.com>
2021-05-04 15:10:47 +0200

    feat: added stuff

---
+++ Skrabbl.Model/Dto/GameLobbyDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Skrabbl.Model.Dto
+{
+    public class GameLobbyDto
+    {
+        public string GameCode { get; set; }
+        public int LobbyOwnerId { get; set; }
+        public List<GameSettingDto> GameSettings { get; set; }
+    }
+}
