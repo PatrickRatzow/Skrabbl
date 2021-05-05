e64531eceb7a4556f8493822abf057ff5752855d
SimonNiese <1086234@ucn.dk>
2021-05-05 14:24:03 +0200

    added turn component to gameview

---
+++ Skrabbl.Web/src/components/Turn.vue
@@ -0,0 +1,24 @@
+<template>
+    <div >
+        <span class="content is-size-4">
+            Round {{ currentRound }} of {{ maxRounds }}  | Time: <span :class="{'has-text-danger': turnTimeLeft < 16}">{{ turnTimeLeft }}</span>
+        </span>
+    </div>
+</template>
+
+<script>
+    import { mapState } from "vuex"
+    export default {
+        name: "turn",
+        computed: {
+            ...mapState("game", {
+                currentRound: "currentRound",
+                turnTimeLeft: "turnTimeLeft",
+                maxRounds: "maxRounds"
+            })
+        }
+    }
+</script>
+
+<style scoped>
+</style>
