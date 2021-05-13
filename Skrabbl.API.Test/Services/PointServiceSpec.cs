2cac2a4443c6e1200cc41502124a24b0c9cd5005
SleepyC4 <nikolajcarlslund@gmail.com>
2021-05-13 19:40:54 +0200

    refactor(test): all test in pointservice now passes

--- Skrabbl.API.Test/Services/PointServiceSpec.cs
+++ Skrabbl.API.Test/Services/PointServiceSpec.cs
@@ -14,8 +14,8 @@ namespace Skrabbl.API.Test.Services
         private List<ChatMessage> messages;
         private List<User> users;
         private Turn turn;
-        private DateTime startTime;
-        private DateTime endTime;
+        DateTime startTime = new DateTime(2021, 05, 13, 17, 35, 5);
+        DateTime endTime;
         private int userId1 = 1;
         private int userId2 = 2;
         private int userId3 = 3;
@@ -30,9 +30,7 @@ namespace Skrabbl.API.Test.Services
             users.Add(CreateUser(userId1));
             users.Add(CreateUser(userId2));
             users.Add(CreateUser(userId3));
-
-            startTime = DateTime.Now;
-            endTime = startTime.AddSeconds(100);
+            endTime = startTime.AddSeconds(500);
 
             turn = new Turn
             {
@@ -85,37 +83,41 @@ namespace Skrabbl.API.Test.Services
             Assert.That(usersPoints, Is.EqualTo(expected));
         }
 
-        [Test]
+        [Test, Order(1)]
         public void TestSortMessagesByTime()
         {
             //Arrange
-            messages.Add(CreateChatMessage(userId1, startTime.AddSeconds(3), "Hest"));
-            messages.Add(CreateChatMessage(userId2, startTime.AddSeconds(30), "Hjort"));
-            messages.Add(CreateChatMessage(userId2, startTime.AddSeconds(40), "Hest"));
-            messages.Add(CreateChatMessage(userId3, startTime.AddSeconds(10), "Hest"));
+            List<ChatMessage> msgList = new List<ChatMessage>();
+
+            msgList.Add(CreateChatMessage(userId1, startTime.AddSeconds(20), "Hest"));
+            msgList.Add(CreateChatMessage(userId2, startTime.AddSeconds(15), "Hjort"));
+            msgList.Add(CreateChatMessage(userId2, startTime.AddSeconds(10), "Hest"));
+            msgList.Add(CreateChatMessage(userId3, startTime.AddSeconds(5), "Hest"));
 
             List<ChatMessage> expectedList = new List<ChatMessage>();
-            expectedList.Add(CreateChatMessage(userId1, startTime.AddSeconds(3), "Hest"));
+            expectedList.Add(CreateChatMessage(userId1, startTime.AddSeconds(5), "Hest"));
             expectedList.Add(CreateChatMessage(userId3, startTime.AddSeconds(10), "Hest"));
-            expectedList.Add(CreateChatMessage(userId2, startTime.AddSeconds(30), "Hjort"));
-            expectedList.Add(CreateChatMessage(userId2, startTime.AddSeconds(40), "Hest"));
+            expectedList.Add(CreateChatMessage(userId2, startTime.AddSeconds(15), "Hjort"));
+            expectedList.Add(CreateChatMessage(userId2, startTime.AddSeconds(20), "Hest"));
 
 
             //Act
-            messages = _service.FilterMessagesByTime(turn, messages);
+            msgList = _service.FilterMessagesByTime(turn, msgList);                
 
             //Assert
-            Assert.That(messages, Is.EqualTo(expectedList));
+            for (int i = 0; i < messages.Count; i++)
+            {
+                Assert.AreEqual(messages[i].CreatedAt, expectedList[i].CreatedAt);
+            }
         }
 
         [Test]
         public void TestAllCorrectGuesses()
         {
             //Arrange
-            messages.Add(CreateChatMessage(userId1, startTime.AddSeconds(3), "Hest"));
-            messages.Add(CreateChatMessage(userId2, startTime.AddSeconds(30), "Hjort"));
-            messages.Add(CreateChatMessage(userId2, startTime.AddSeconds(40), "Hest"));
-            messages.Add(CreateChatMessage(userId3, startTime.AddSeconds(10), "Hest"));
+            messages.Add(CreateChatMessage(userId2, startTime.AddSeconds(10), "Hest"));
+            messages.Add(CreateChatMessage(userId3, startTime.AddSeconds(5), "Hest"));
+            messages.Add(CreateChatMessage(userId1, startTime.AddSeconds(10), "Hest"));
 
             //Act
             messages = _service.CorrectGuesses(turn, messages);
@@ -197,7 +199,7 @@ namespace Skrabbl.API.Test.Services
             messages.Add(CreateChatMessage(userId1, startTime.AddSeconds(3), "Hest"));
 
             List<int> expected = new List<int>();
-            expected.Add(1050);
+            expected.Add(50);
             expected.Add(0);
             expected.Add(0);
 
